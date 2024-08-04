eaa1ef1ddb4b4b9f68002cc09d9bfbac25d45f00
Nile Okomo <okomonile3@gmail.com>
2024-08-04 18:50:28 +0300

    Update redundant code

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-import base64
-import folium
-import pandas as pd
-import plotly.express as px
 import streamlit as st
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import hashes
-from firebase_admin import credentials, db
 from menu import menu
-from streamlit_folium import st_folium
 
 if "login" not in st.session_state:
     st.session_state.login = False
