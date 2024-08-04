c1ea2543906067ac5f09601d5d845a4e9864c3a9
Nile Okomo <okomonile3@gmail.com>
2024-08-04 18:53:41 +0300

    Remove redundant code

--- utilities/auth.py
+++ utilities/auth.py
@@ -7,7 +7,6 @@ from firebase_admin import credentials, auth
 
 load_dotenv()
 
-# Initialize Firebase
 if not firebase_admin._apps:
     cred = credentials.Certificate('servicekey.json')
     firebase_admin.initialize_app(cred, {
